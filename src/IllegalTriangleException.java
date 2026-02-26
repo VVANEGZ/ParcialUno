2784a482bb24dd4822831021db87198b4e1d92eb
GARCIA ZARATE VERONICA VANESA <zS24016699@estudiantes.uv.mx>
2026-02-25 18:00:48 -0600

    pequeños cambios

--- src/IllegalTriangleException.java
+++ src/IllegalTriangleException.java
@@ -1,12 +1,5 @@
-import java.util.InputMismatchException;
-
-public class IllegalTriangleException {
-
-    /*public void setEdad(int edad) {
-        if (side1 + side2> side3 || edad > 150) {
-            throw new IllegalArgumentException("Triángulo infrige la regla.");
-        }
-        this.edad = edad;
+public class IllegalTriangleException extends Exception{
+    public IllegalTriangleException(String message) {
+        super(message);
     }
-*/
 }
--- src/Triangle.java
+++ src/Triangle.java
@@ -30,4 +30,9 @@ public class Triangle extends GeometricObject{
     public String toString(){
         return "Triángulo: side 1= " + side1 + ", side 2= " + side2 + ", side 3= " + side3;
     }
+    static void IllegalTriangleException(double side1, double side2, double side3) throws IllegalArgumentException{
+        if(side1 + side2 <= side3 && side2 + side3 <= side1){
+            throw new IllegalArgumentException("Los lados no forman un triángulo válido.");
+        }
+    }
 }
